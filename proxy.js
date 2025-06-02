480ce12f52918a8f1ce5cf8dc58adcb3462727e0
JuanJo Andrade <juanjoandpe@hotmail.com>
2025-06-02 15:50:23 -0500

    cambio headers

--- proxy.js
+++ proxy.js
@@ -74,6 +74,7 @@ app.post('/proxy', authenticateJWT, async (req, res) => {
       headers: {
         'Authorization': 'Basic ' + Buffer.from('WSTEST-TAQTICA:1Ex#YXTbaK').toString('base64'),
         'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
       }
     });
     res.json(response.data);

d294b29ea83927cea580ed97afc1490f31a3ed94
JuanJo Andrade <juanjoandpe@hotmail.com>
2025-06-02 16:40:00 -0500

    cambios users

--- proxy.js
+++ proxy.js
@@ -13,14 +13,14 @@ app.use(express.json());
 const users = [
   {
     id: 1,
-    username: process.env.ADMIN_USER || 'admin',
-    passwordHash: bcrypt.hashSync(process.env.ADMIN_PASSWORD || 'admin123', 10),
+    username: process.env.ADMIN_USER,
+    passwordHash: bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10),
     role: 'admin'
   },
   {
     id: 2,
-    username: process.env.ANALYST_USER || 'analista',
-    passwordHash: bcrypt.hashSync(process.env.ANALYST_PASSWORD || 'analista123', 10),
+    username: process.env.ANALYST_USER,
+    passwordHash: bcrypt.hashSync(process.env.ANALYST_PASSWORD, 10),
     role: 'analyst'
   }
 ];
